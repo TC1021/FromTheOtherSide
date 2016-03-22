00030af5e91a20c886ce5373585a9ab40996368c
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-03-21 21:23:33 -0600

    Added AudioProcessor to detect Beat, testing BeatIndicator switching, added tags to those Indicators

---
+++ Assets/Scripts/BeatDetector.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System;
+
+/*
+ * Make your class implement the interface AudioProcessor.AudioCallbaks
+ */
+using System.Collections;
+
+
+public class BeatDetector : MonoBehaviour, AudioProcessor.AudioCallbacks
+{
+	protected GameObject iOnBeat, iDefault,iError;
+
+    void Start()
+    {
+		iDefault= GameObject.Find("beat_marker_red");
+		iOnBeat= GameObject.Find("beat_marker_green"); iOnBeat.SetActive (false);
+		iError= GameObject.Find("beat_marker");	   iError.SetActive (false);
+		//SET AUDIOCALLBACK
+        AudioProcessor processor = FindObjectOfType<AudioProcessor>();
+        processor.addAudioCallback(this);
+    }
+	IEnumerator wait(float seconds){yield return new WaitForSeconds(seconds); }
+    
+	public void onOnbeatDetected()
+    {
+		Debug.Log("BEAT");
+		activate (iOnBeat);
+		deactivate (iDefault);
+		wait (1.0f);
+		deactivate (iOnBeat);
+		activate (iDefault);
+    }
+
+	public void deactivate(GameObject x){x.SetActive (false);}
+	public void activate(GameObject x){x.SetActive (true);}
+
+    //This event will be called every frame while music is playing
+    public void onSpectrum(float[] spectrum)
+    {
+        //The spectrum is logarithmically averaged
+        //to 12 bands
+
+        for (int i = 0; i < spectrum.Length; ++i)
+        {
+            Vector3 start = new Vector3(i, 0, 0);
+            Vector3 end = new Vector3(i, spectrum[i], 0);
+            Debug.DrawLine(start, end);
+        }
+    }
+}

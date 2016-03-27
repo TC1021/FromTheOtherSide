bd02d991eb99ecab9247d0be8796530cac88902a
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-03-27 14:57:25 -0600

    Added Camera controller to follow player,Player controller to move player, not working as I wish but working

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public GameObject player;
+	private Vector3 offset;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position - player.transform.position;
+	}
+	
+	//RUNS AFTER ALL ITEM HAS BEEN PROCESSED
+	void LateUpdate () 
+	{
+		transform.position = player.transform.position + offset;	
+	}
+}

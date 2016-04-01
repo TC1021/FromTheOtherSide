fe61999cad0e2041849e2e15d5e9971849fa7b26
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-03-31 20:28:53 -0600

    Added player controller as well added GUI elements to camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,18 +3,44 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	private GameObject onBeat, notOnBeat;
+	private GameObject[] half_hearths;
+	private bool move;
+	public short life;
+
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
+	void Start () 
 	{
 
-		if (Input.GetAxis ("Horizontal") > 0) //MoverDerecha
-			return;
-		else if (Input.GetAxis ("Horizontal") < 0) //MoverIzq
-			return;
+		onBeat = GameObject.Find ("beat_marker_green");
+		notOnBeat = GameObject.Find ("beat_marker_red");
+		move = true;
+		life = 10; //In HalfHearts
+		half_hearths = GameObject.FindGameObjectsWithTag("Indicator");
+		updateLifeBar();
+	}
+	
+	// Movements
+	void Update () 
+	{
+		if (onBeat.activeSelf && move) 
+		{
+			if (Input.GetKeyDown(KeyCode.UpArrow))
+				transform.position= transform.position+ new Vector3(0,1,0);
+			if (Input.GetKeyDown(KeyCode.DownArrow))
+				transform.position= transform.position+ new Vector3(0,-1,0);
+			if (Input.GetKeyDown(KeyCode.LeftArrow))
+				transform.position= transform.position+ new Vector3(-1,0,0);
+			if (Input.GetKeyDown(KeyCode.RightArrow))
+				transform.position= transform.position+ new Vector3(1,0,0);
+		}
+		else if (notOnBeat.activeSelf) move = true;
+	}
+	public void updateLifeBar()
+	{
+		SpriteRenderer h = half_hearths[2].GetComponent<SpriteRenderer> ();
+		h.sprite = Resources.Load("/GUI/heart_half.png") as Sprite;
+
+		//GameObject.FindGameObjectsWithTag ();
 	}
 }

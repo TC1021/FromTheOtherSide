6dc10ab32b6e18ba5c612d2bfe4eb178e8be2d88
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-04-03 00:46:49 -0600

    Agregado script del enemigo, por el momento solo calcula distancia entre jugador/enemigo, falta implementar toma de decisiones

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	GameObject player;
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag("Player") ;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Si la magnitud es menor a...5? deberia perseguirlo, de otra manera quedarse quieto o moverse random 1 lugar cada x segundos
+		//Debug.Log ( (transform.position-player.transform.position).magnitude ); 
+	}
+}

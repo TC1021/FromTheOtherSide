e510fa99fc1867065420e844c4d70509ae2572e5
Seung Lee <sintaelz@gmail.com>
2016-05-05 16:11:35 -0500

    ahahaha

---
+++ Assets/Scripts/LoadAdditive.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadAdditive : MonoBehaviour {
+    public string level;
+
+    public void AddOnClick(){
+		SceneManager.LoadScene (level);
+	}
+}

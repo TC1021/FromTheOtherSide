5483af2d38f0d6e335d7a8b62824ce725c8de1c8
Gualberto Casas Medina <melissacasas@Gualbertos-iMac.local>
2016-05-11 18:54:17 -0500

    arreglé los niveles, solo que siguen estando bien cabrones para ser los primero

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -1,15 +1,43 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityEngine.SceneManagement;
+//
+//public class LoadOnClick : MonoBehaviour {
+//
+//	public void LoadScene(string level){
+//		SceneManager.LoadScene(level);
+//	}
+//    
+//    public void exitGame()
+//    {
+//        Application.Quit();
+//    }
+//}
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
 public class LoadOnClick : MonoBehaviour {
+	void start()
+	{
+		GameObject gamemanager = GameObject.Find("GameManager");
+		GameManager gameManagerScript = gamemanager.GetComponent<GameManager>();
+		gameManagerScript.level = 0;
+	}
 
 	public void LoadScene(string level){
+		GameObject[] GameObjects = (FindObjectsOfType<GameObject>() as GameObject[]);
+		for (int i = 0; i < GameObjects.Length; i++)
+		{
+			Destroy(GameObjects[i]);
+		}
+		//gameManagerScript.level = 1;
 		SceneManager.LoadScene(level);
 	}
-    
-    public void exitGame()
-    {
-        Application.Quit();
-    }
+
+	public void exitGame()
+	{
+		Application.Quit();
+	}
 }

74f7e41dfb27a5de85441ac53b6d80eee799476a
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-05-11 16:52:13 -0500

    new script

--- Assets/Scripts/dragonController.cs
+++ Assets/Scripts/dragonController.cs
@@ -5,8 +5,11 @@ public class dragonController : EnemyController
 {
 	protected override bool tryToattack(int xDir,int yDir)
 	{ 
-		if ((target.transform.position-transform.position).magnitude<2) //Dragon ataca desde mas lejos
+		float k = (target.transform.position - transform.position).magnitude;
+		Debug.Log (k);
+		if (k<2) //Dragon ataca desde mas lejos
 		{
+			
 			animator.SetTrigger("attack");
 			target.GetComponent<Player>().looseHealth(playerDamage);
 			return true;
